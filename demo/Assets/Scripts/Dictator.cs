bc89fec4408eedec9a6200910a19e4447c272a05
prakashph <prakashph@users.noreply.github.com>
2018-04-11 23:41:37 -0700

    change voice

---
+++ demo/Assets/Scripts/Dictator.cs
@@ -0,0 +1,32 @@
+﻿using HoloToolkit.Unity.InputModule;
+using Microsoft.MR.LUIS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Dictator : AttachToController {
+
+    public LuisDictationManager manager;
+    protected override void OnAttachToController()
+    {
+        base.OnAttachToController();
+        InteractionManager.InteractionSourceUpdated += InteractionSourceUpdated;
+        Debug.Log("Attached to controler");
+    }
+
+    private void InteractionSourceUpdated(InteractionSourceUpdatedEventArgs obj)
+    {
+        if (obj.state.touchpadPressed)
+        {
+            manager.StartListening();
+        }
+    }
+
+    protected override void OnDetachFromController()
+    {
+        base.OnAttachToController();
+        InteractionManager.InteractionSourceUpdated -= InteractionSourceUpdated;
+    }
+}
